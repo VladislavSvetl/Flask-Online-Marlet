e9edcf76085092549de692949faef883efa0148f
svetl <v.svetlichniy1999@gmail.com>
2022-12-26 17:17:44 +0300

    Created database 'market.db' for items on online market

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    tags = db.Column(db.String(75), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 @app.route('/')
